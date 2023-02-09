9f5054571ab5c7a90ea746b6ba9e576ac7d5d7c6
Jean Rubio <jeanrubiomc@gmail.com>
2023-02-09 12:21:03 -0500

    agregacion con puertos GPIO

--- probando.py
+++ probando.py
@@ -2,6 +2,29 @@ import cv2
 import mediapipe as mp
 import numpy as np
 import time
+import RPi.GPIO as GPIO
+
+PIN_AZUL = 38
+PIN_VERDE = 40
+PIN_ROJO = 36
+PIN_ALARMA = 37
+PIN_ALARMA2 = 35
+
+GPIO.setmode(GPIO.BOARD)
+
+
+def visual_alarm(pin, tiempo):
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.HIGH)
+    time.sleep(tiempo)
+
+
+def sound_alarm(pin, tiempo):
+    GPIO.setmode(GPIO.BOARD)
+    GPIO.setup(pin, GPIO.OUT)
+    GPIO.output(pin, GPIO.HIGH)
+    time.sleep(tiempo)
 
 
 def eye_aspect_ratio(coordinates):
@@ -33,7 +56,7 @@ inicio = 0
 
 mp_face_mesh = mp.solutions.face_mesh
 mp_dibujo = mp.solutions.drawing_utils
-ConfDibu = mp_dibujo.DrawingSpec(thickness=1, circle_radius=1)
+# ConfDibu = mp_dibujo.DrawingSpec(thickness=1, circle_radius=1)
 index_left_eye = [33, 160, 158, 133, 153, 144]
 index_right_eye = [362, 385, 387, 263, 373, 380]
 index_boquita = [61, 39, 0, 269, 291, 405, 17, 181]
@@ -47,7 +70,7 @@ with mp_face_mesh.FaceMesh(
         ret, frame = cap.read()
         if ret == False:
             break
-        frame = cv2.flip(frame, 1)
+        frame = cv2.resize(frame, (480, 320))
         height, width, _ = frame.shape
         frame_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
         results = face_mesh.process(frame_rgb)
@@ -58,8 +81,8 @@ with mp_face_mesh.FaceMesh(
         if results.multi_face_landmarks is not None:
             for face_landmarks in results.multi_face_landmarks:
 
-                mp_dibujo.draw_landmarks(
-                    frame, face_landmarks, mp_face_mesh.FACEMESH_CONTOURS, ConfDibu, ConfDibu)
+                # mp_dibujo.draw_landmarks(
+                # frame, face_landmarks, mp_face_mesh.FACEMESH_CONTOURS, ConfDibu, ConfDibu)
                 for index in index_left_eye:
                     x = int(face_landmarks.landmark[index].x * width)
                     y = int(face_landmarks.landmark[index].y * height)
@@ -84,36 +107,38 @@ with mp_face_mesh.FaceMesh(
                 mar_boquita = boquita_aspect_ratio(coordinates_boquita)
                 ear = (ear_left_eye+ear_right_eye)/2
                 mar = mar_boquita
-                # print("MAR:", mar)
-                # print("EAR izquierdo:", ear_left_eye,
-                # "EAR derecho:", ear_right_eye)
-                # print("EAR: ", ear)
-                # OJOS CERRADOS O MEDIOS CERRADOS
-                # count time when eyes are closed for more than 3 seconds and then alert
 
+ # ALERTA PARA OJOS
                 if ear < EAR_THRESH:
                     inicio = time.time()
-                    # print("Inicio:", inicio)
                 elif ear > EAR_THRESH:
                     fin = time.time()
-                    # print("Fin:", fin)
 
                 tiempo = round(inicio-fin, 0)
                 print("Tiempo:", tiempo)
-                if tiempo > 3:
-                    cv2.putText(frame, mensaje, (300, 60),
-                                cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 0, 0), 3)
 
-                # if ear < EAR_THRESH:
-                # cv2.putText(frame, mensaje, (300, 60),
-                # cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 0, 0), 3)
+                if tiempo > 3 and tiempo <= 5:
+                    visual_alarm(PIN_AZUL, 0.05)
+                    sound_alarm(PIN_ALARMA, 0.05)
+                    cv2.putText(frame, mensaje, (240, 60),
+                                cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 0, 255), 3)
+                if tiempo > 5:
+                    visual_alarm(PIN_ROJO, 0.05)
+                    sound_alarm(PIN_ALARMA, 0.05)
+                    sound_alarm(PIN_ALARMA2, 0.05)
+                    cv2.putText(frame, mensaje, (240, 60),
+                                cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 0, 255), 3)
 
-                # print("Inicio:", inicio)
-                # BOCA ABIERTA
+                GPIO.cleanup()
+                # ALERTA BOCA ABIERTA
                 if mar > MAR_THRESH:
-                    cv2.putText(frame, mensaje, (300, 60),
-                                cv2.FONT_HERSHEY_TRIPLEX, 1, (255, 0, 0), 3)
+                    visual_alarm(PIN_AZUL, 0.05)
+                    sound_alarm(PIN_ALARMA, 0.05)
+                    sound_alarm(PIN_ALARMA2, 0.05)
+                    cv2.putText(frame, mensaje, (240, 60),
+                                cv2.FONT_HERSHEY_TRIPLEX, 1, (0, 0, 255), 3)
 
+                GPIO.cleanup()
         cv2.imshow("Frame", frame)
         k = cv2.waitKey(1) & 0xFF
         if k == 27:
